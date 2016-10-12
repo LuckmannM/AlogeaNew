5246b74f3ebe8d9a7eeed6b1096df6aa050744bf
mikeMBP <mikeMBP@mbpro-ci7.local>
2016-10-12 10:29:20 +0100

    TouchWheelView and constraints for all portraits ok

---
+++ Alogea/TouchWheelView.swift
@@ -0,0 +1,21 @@
+//
+//  TouchWheelView.swift
+//  Alogea
+//
+//  Created by mikeMBP on 12/10/2016.
+//  Copyright © 2016 AppToolFactory. All rights reserved.
+//
+
+import UIKit
+
+class TouchWheelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
