b1adf8475055eddca5d273ae398060640f1eabd7
mikeMBP <mikeMBP@mbpro-ci7.local>
2016-10-12 10:24:44 +0100

    Added GraphContainer UiView and colorscheme class with all colors

---
+++ Alogea/GraphContainer.swift
@@ -0,0 +1,21 @@
+//
+//  GraphContainer.swift
+//  Alogea
+//
+//  Created by mikeMBP on 10/10/2016.
+//  Copyright © 2016 AppToolFactory. All rights reserved.
+//
+
+import UIKit
+
+class GraphContainer: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
