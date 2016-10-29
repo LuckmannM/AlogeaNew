e085160c9d933af8506980f02cc04758631aa9f9
mikeMBP <mikeMBP@mbpro-ci7.local>
2016-10-29 19:49:01 +0100

    Added ClipView

---
+++ Alogea/ClipView.swift
@@ -0,0 +1,21 @@
+//
+//  ClipView.swift
+//  Alogea
+//
+//  Created by mikeMBP on 29/10/2016.
+//  Copyright © 2016 AppToolFactory. All rights reserved.
+//
+
+import UIKit
+
+class ClipView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
