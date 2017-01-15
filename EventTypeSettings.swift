ee8f9186d46361d27fb2eefbb70387b2204ffb0b
mikeMBP <mikeMBP@des.local>
2017-01-15 21:49:50 +0000

    First version

---
+++ EventTypeSettings.swift
@@ -0,0 +1,175 @@
+//
+//  EventTypeSettings.swift
+//  Alogea
+//
+//  Created by mikeMBP on 14/01/2017.
+//  Copyright © 2017 AppToolFactory. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class EventTypeSettings: UITableViewController {
+    
+    var stack: CoreDataStack!
+    var rootViewController: UIViewController!
+    
+    var recordTypesController: RecordTypesController!
+    var eventsController: EventsDataController!
+    
+    let titleTag = 10
+    let subTitleTag = 20
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        let moc = (UIApplication.shared.delegate as! AppDelegate).stack.context
+        return moc
+    }()
+    
+    lazy var nonScoreEventTypesFRC: NSFetchedResultsController<Event> = {
+        let request = NSFetchRequest<Event>(entityName: "Event")
+        let predicate = NSPredicate(format: "type == %@", argumentArray: ["Diary Entry"])
+        request.predicate = predicate
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: false), NSSortDescriptor(key: "date", ascending: true)]
+        let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: "name", cacheName: nil)
+        
+        do {
+            try frc.performFetch()
+        } catch let error as NSError{
+            print("nonScoreEventTypesFRC fetching error \(error)")
+        }
+        
+        return frc
+    }()
+
+    lazy var recordTypes: NSFetchedResultsController<RecordType> = {
+        let request = NSFetchRequest<RecordType>(entityName: "RecordType")
+        request.sortDescriptors = [NSSortDescriptor(key: "dateCreated", ascending: true)]
+        let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try frc.performFetch()
+        } catch let error as NSError{
+            print("allRecordTypesFRC fetching error")
+        }
+        
+        return frc
+    }()
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        print("load EventTypeSettings VC, scoreEventTypesFRC has \(EventsDataController.sharedInstance().scoreEventsFRC.fetchedObjects?.count ?? 0) objects")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+//            print("recordTypesController.recordTypeNames are \(recordTypesController.recordTypeNames)")
+            return recordTypes.fetchedObjects?.count ?? 0
+        } else {
+//            print("eventsController.nonScoreEventTypes are \(eventsController.nonScoreEventTypes)")
+            return nonScoreEventTypesFRC.fetchedObjects?.count ?? 0
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventTypeCell", for: indexPath)
+
+        if indexPath.section == 0 {
+            (cell.contentView.viewWithTag(titleTag) as! UILabel).text = recordTypes.object(at: indexPath).name!
+        } else {
+            (cell.contentView.viewWithTag(titleTag) as! UILabel).text = nonScoreEventTypesFRC.object(at: indexPath).name!
+            
+        }
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return "Symptoms"
+        } else {
+            return "Event categories"
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        let header = view as! UITableViewHeaderFooterView
+        let textSize: CGFloat = 22 * (UIApplication.shared.delegate as! AppDelegate).deviceBasedSizeFactor.width
+        header.textLabel?.font = UIFont(name: "AvenirNext-Regular", size: textSize)
+        header.textLabel?.sizeToFit()
+        
+        header.textLabel?.textColor = UIColor.darkGray
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SettingsViewController.swift
@@ -0,0 +1,225 @@
+//
+//  SettingsViewController.swift
+//  Alogea
+//
+//  Created by mikeMBP on 14/01/2017.
+//  Copyright © 2017 AppToolFactory. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Foundation
+
+class SettingsViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    // MARK: - Class variables
+    
+    @IBOutlet weak var cloudSwitch: UISwitch!
+    @IBOutlet weak var drugReminderSwitch: UISwitch!
+
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        let moc = (UIApplication.shared.delegate as! AppDelegate).stack.context
+        return moc
+    }()
+    
+    lazy var stack: CoreDataStack = {
+        return (UIApplication.shared.delegate as! AppDelegate).stack
+    }()
+    
+    lazy var recordTypesFRC : NSFetchedResultsController<RecordType> = {
+        let request = NSFetchRequest<RecordType>(entityName: "RecordType")
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: false)]
+        
+        let fetch = NSFetchedResultsController<RecordType>(fetchRequest: request, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetch.delegate = self
+        return fetch
+    }()
+    
+    var recordTypesController = RecordTypesController.sharedInstance()
+
+    
+    // MARK: - View functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        drugReminderSwitch.isOn = UserDefaults.standard.bool(forKey: "DrugReminderNotification")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.hidesBottomBarWhenPushed = false
+        self.tabBarController!.tabBar.isHidden = false
+        
+        cloudSwitch.isOn = UserDefaults.standard.bool(forKey: "iCloudBackups")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Custom functions
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ 
+        switch section {
+        case 0 :
+            return 0
+        case 1 :
+            return 1
+        case 2 :
+            return 2
+        case 3 :
+            return 1
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        switch section {
+        case 0 :
+            return "Alogea ® Version " + (UIApplication.shared.delegate as! AppDelegate).appVersion + ", Build " + (UIApplication.shared.delegate as! AppDelegate).appBuild
+        case 1 :
+            return ""
+        case 2 :
+            return "Backup / Restore options"
+        case 3 :
+            return "switch on/off timed medicine reminders"
+        default:
+            return ""
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func persistentStoreCoordinatorDidChangeStores(notification: NSNotification) {
+        
+        do {
+            try recordTypesFRC.performFetch()
+        } catch let error as NSError {
+            print("Error fetching notes: \(error)")
+        }
+        
+    }
+    
+    @IBAction func cloudSwitchAction(sender: UISwitch) {
+        
+        UserDefaults.standard.set(sender.isOn, forKey: "iCloudBackups")
+        if !sender.isOn {
+            removeCloudBackupsDialog(sender: sender)
+        }
+        
+    }
+    
+    func removeCloudBackupsDialog(sender: UISwitch) {
+        
+        let optionsDialog = UIAlertController(title: "Keep or delete all iCloud backups?", message: "When switching off iCloud storage you can keep existing Cloud backups for later access or delete them all now", preferredStyle: .actionSheet)
+        
+        let keepAction = UIAlertAction(title: "Keep backups", style: UIAlertActionStyle.default, handler: { (optionsDialog)
+            -> Void in
+            
+            return
+        })
+        
+        optionsDialog.addAction(keepAction)
+        
+        
+        let deleteAction = UIAlertAction(title: "Delete all iCloud backups", style: .default, handler: { (exportDialog)
+            -> Void in
+            
+// ***            DataIOBackUp.deleteCloudBackups()
+        })
+        
+        optionsDialog.addAction(deleteAction)
+        
+        
+        if UIDevice().userInterfaceIdiom == .pad {
+            let popUpController = optionsDialog.popoverPresentationController
+            popUpController!.permittedArrowDirections = .right
+            popUpController?.sourceView = sender
+            popUpController?.sourceRect = sender.bounds
+        }
+        
+        
+        self.present(optionsDialog, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func drugReminderSwitchAction(sender: UISwitch) {
+        
+        
+        UserDefaults.standard.set(sender.isOn, forKey: "DrugReminders")
+        
+        
+        // Switch off  - cancel all pending notifications for drugs
+        if !sender.isOn {
+            (UIApplication.shared.delegate as! AppDelegate).removeCategoryNotifications(withCategory: "DrugReminderCategory")
+            
+        }
+        else {
+            // Switch on - recreate notifications for drugs that have reminders set
+            let fetchRequest = NSFetchRequest<DrugEpisode>(entityName: "DrugEpisode")
+            var drugsList = [DrugEpisode]()
+            
+            do {
+                drugsList = try managedObjectContext.fetch(fetchRequest)
+            } catch let error as NSError {
+                print("SettingsVC_DrugReminderSwitchAction - error fetching drugList for reminderSWitch\(error)")
+            }
+            
+            for drug in drugsList {
+                // for some reason the iteration doesn't trigger awakeFromFetch for the drug object
+                // the non-initiated local DrugEpisode variables can trigger crashes
+                drug.convertFromStorage()
+                drug.scheduleReminderNotifications(cancelExisting: false)
+            }
+        }
+        
+        
+    }
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "eventTypesSegue"
+        {
+            if let nextViewController = segue.destination as? EventTypeSettings {
+                nextViewController.stack = stack
+                nextViewController.rootViewController = self
+                nextViewController.recordTypesController = recordTypesController
+                nextViewController.eventsController = EventsDataController.sharedInstance()
+
+            } else {
+                print("error createNew segue: destinationViewcontrolle not defined")
+            }
+            
+        } else {
+            print("non-identified  segue with id \(segue.identifier)")
+            
+        }
+    }
+    
+    
+}
+
