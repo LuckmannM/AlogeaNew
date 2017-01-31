05e34330d596b61d0a882d217b9f4ba392c12343
mikeMBP <mikeMBP@des.local>
2017-01-31 21:21:11 +0000

    Working and cleaned copy introduced symbolArrays for use in touch events

--- Alogea/MedsView.swift
+++ Alogea/MedsView.swift
@@ -14,6 +14,10 @@ let eventDiamondSize: CGFloat = 15.0 * (UIApplication.shared.delegate as! AppDel
 
 class MedsView: UIView {
     
+    let cornerRadius: CGFloat = 8.0 / 2
+    let fontSize: CGFloat = 11 * (UIApplication.shared.delegate as! AppDelegate).deviceBasedSizeFactor.height
+
+    
     var graphView: GraphView!
     var helper: GraphViewHelper!
     var medController: MedicationController!
@@ -29,6 +33,12 @@ class MedsView: UIView {
     var diaryEventsFRC: NSFetchedResultsController<Event> {
         return EventsDataController.sharedInstance().nonScoreEventsFRC
     }
+    
+    var symbolArrays = [[CGRect]]()
+    var count = 0
+    var colorArrayCount = 0
+    let numberOfColors = ColorScheme.sharedInstance().barColors.count
+
 
     
     // MARK: - Init
@@ -47,6 +57,7 @@ class MedsView: UIView {
         self.backgroundColor = UIColor.clear
         
         self.medController = MedicationController.sharedInstance()
+        
         print("...init MedsView end")
 
     }
@@ -59,36 +70,41 @@ class MedsView: UIView {
     
     override func draw(_ rect: CGRect) {
         
-        let scale = graphView.maxDisplayDate.timeIntervalSince(graphView.minDisplayDate) / TimeInterval(graphView.frame.width)
-        let cornerRadius: CGFloat = 8.0 / 2
-        let fontSize: CGFloat = 11 * (UIApplication.shared.delegate as! AppDelegate).deviceBasedSizeFactor.height
-        
-        let verticalOffset = bounds.maxY - helper.timeLineSpace()
-        var topOfRegMedRects = verticalOffset // to find vertical lowest position for prnMedRect
-        
         UIColor.white.setStroke()
+        symbolArrays.removeAll()
+
+        let scale = graphView.maxDisplayDate.timeIntervalSince(graphView.minDisplayDate) / TimeInterval(graphView.frame.width)
+        
+        let topOfRegMedRects = drawRegularMeds(scale: scale, verticalOffset: bounds.maxY - helper.timeLineSpace())
+        
+        let topOfPrnMedRects = drawPrnMeds(scale: scale, verticalOffset: topOfRegMedRects)
+        
+        drawNonScoreEvents(scale: scale, verticalOffset: topOfPrnMedRects)
+        
+        // symbol Arrays contains all med and eventRects for use in touch events
+        
+    }
+    
+    private func drawRegularMeds(scale: TimeInterval, verticalOffset: CGFloat) -> CGFloat {
         
         var rectArray = [CGRect]()
-        
-        
-// draw regular medicines rects and names
-        var count = 0
-        var colorArrayCount = 0
-        var numberOfColors = ColorScheme.sharedInstance().barColors.count
+        var topOfRect = verticalOffset
+        count = 0
+        colorArrayCount = 0
         for medicine in regMedsFRC.fetchedObjects! {
             var medRect = medicine.medRect(scale: scale).offsetBy(dx: leftXPosition(startDate: medicine.startDate as! Date, scale: scale), dy: verticalOffset)
             
             for i in 0..<count {
                 if medRect.intersects(rectArray[i]) {
                     medRect = medRect.offsetBy(dx: 0, dy: -medBarHeight - 2)
-                    if medRect.minY < topOfRegMedRects {
+                    if medRect.minY < topOfRect {
                         // ensure prnMedRect are positoned higher than regMedRects
-                        topOfRegMedRects = medRect.minY - medBarHeight - 2
+                        topOfRect = medRect.minY - medBarHeight - 2
                     }
                 }
             }
             rectArray.append(medRect)
-
+            
             let medRectPath = UIBezierPath(roundedRect: medRect, cornerRadius: cornerRadius)
             medRectPath.lineWidth = 1.0
             ColorScheme.sharedInstance().barColors[colorArrayCount].setFill()
@@ -102,26 +118,30 @@ class MedsView: UIView {
                 colorArrayCount = 0
             }
         }
+        symbolArrays.append(rectArray)
+        return topOfRect
+    }
+    
+    private func drawPrnMeds(scale: TimeInterval, verticalOffset: CGFloat) -> CGFloat {
         
-// draw medicine events (prn meds) rects and names
-        var topOfPrnMedRect = topOfRegMedRects
-        rectArray = [CGRect]()
+        var topOfRect = verticalOffset
+        var rectArray = [CGRect]()
+        
         count = 0
         colorArrayCount = 0
-        
         var sectionCount = 0
         var eventCount = 0
+        
         for section in prnMedsFRC.sections! {
             for index in 0..<section.numberOfObjects {
                 let medEvent = prnMedsFRC.object(at: IndexPath(item: index, section: sectionCount))
-                print("prn Med index \(index), section \(sectionCount), name \(medEvent.name!)")
-                var medRect = medEvent.medEventRect(scale: scale).offsetBy(dx: leftXPosition(startDate: medEvent.date as! Date, scale: scale), dy: topOfRegMedRects)
+                var medRect = medEvent.medEventRect(scale: scale).offsetBy(dx: leftXPosition(startDate: medEvent.date as! Date, scale: scale), dy: verticalOffset)
                 for i in 0..<eventCount {
                     if medRect.intersects(rectArray[i]) {
                         medRect = medRect.offsetBy(dx: 0, dy: -medBarHeight - 2)
-                        if medRect.minY < topOfPrnMedRect {
+                        if medRect.minY < topOfRect {
                             // ensure prnMedRect are positoned higher than regMedRects
-                            topOfPrnMedRect = medRect.minY - medBarHeight - 2
+                            topOfRect = medRect.minY - medBarHeight - 2
                         }
                     }
                 }
@@ -149,66 +169,37 @@ class MedsView: UIView {
             }
             sectionCount += 1
         }
-        
-//        for medEvent in prnMedsFRC.fetchedObjects! {
-//            var medRect = medEvent.medEventRect(scale: scale).offsetBy(dx: leftXPosition(startDate: medEvent.date as! Date, scale: scale), dy: topOfRegMedRects)
-//            
-//            // ensure medRects don't overlap, otherwise shift rect up by 5
-//            for i in 0..<count {
-//                if medRect.intersects(rectArray[i]) {
-//                    medRect = medRect.offsetBy(dx: 0, dy: -medBarHeight - 2)
-//                    if medRect.minY < topOfPrnMedRect {
-//                        // ensure prnMedRect are positoned higher than regMedRects
-//                        topOfPrnMedRect = medRect.minY - medBarHeight - 2
-//                    }
-//                }
-//            }
-//            rectArray.append(medRect)
-//            
-//            let medRectPath = UIBezierPath(roundedRect: medRect, cornerRadius: cornerRadius)
-//            medRectPath.lineWidth = 1.0
-//            ColorScheme.sharedInstance().barColors[colorArrayCount].setFill()
-//            medRectPath.fill()
-//            medRectPath.stroke()
-//            
-//            var medName = NSString()
-//            if medRect.width == medBarHeight {
-//                medName = (medEvent.name! as NSString).substring(to: 1) as NSString
-//            } else {
-//                medName = medEvent.name! as NSString
-//            }
-//            medName.draw(in: medRect.offsetBy(dx: 3, dy: -1), withAttributes: [NSFontAttributeName: UIFont(name: "AvenirNext-Regular", size: fontSize)!,NSForegroundColorAttributeName: UIColor.white])
-//            
-//            count += 1
-//            colorArrayCount += 1
-//            if colorArrayCount == numberOfColors {
-//                colorArrayCount = 0
-//            }
-//        }
+        symbolArrays.append(rectArray)
+        return topOfRect
+    }
+    
+    private func drawNonScoreEvents(scale: TimeInterval, verticalOffset: CGFloat)  {
         
         count = 0
-        rectArray = [CGRect]()
-        sectionCount = 0
-        eventCount = 0
+        var rectArray = [CGRect]()
+        
+        var sectionCount = 0
+        var eventCount = 0
         colorArrayCount = 0
+        
         for section in diaryEventsFRC.sections! {
             for index in 0..<section.numberOfObjects {
                 let event = diaryEventsFRC.object(at: IndexPath(item: index, section: sectionCount))
-                var eventRect = event.nonScoreEventRect(scale: scale).offsetBy(dx: leftXPosition(startDate: event.date as! Date, scale: scale), dy: topOfPrnMedRect)
+                var eventRect = event.nonScoreEventRect(scale: scale).offsetBy(dx: leftXPosition(startDate: event.date as! Date, scale: scale), dy: verticalOffset)
                 for i in 0..<eventCount {
                     if eventRect.intersects(rectArray[i]) {
                         eventRect = eventRect.offsetBy(dx: 0, dy: -eventDiamondSize - 2)
                     }
                 }
                 rectArray.append(eventRect)
-    
+                
                 let diamondPath = UIBezierPath()
                 diamondPath.move(to: CGPoint(x:eventRect.midX, y: eventRect.minY))
                 diamondPath.addLine(to: CGPoint(x:eventRect.minX, y: eventRect.midY))
                 diamondPath.addLine(to: CGPoint(x:eventRect.midX, y: eventRect.maxY))
                 diamondPath.addLine(to: CGPoint(x:eventRect.maxX, y: eventRect.midY))
                 diamondPath.addLine(to: CGPoint(x:eventRect.midX, y: eventRect.minY))
-    
+                
                 diamondPath.lineWidth = 1.0
                 ColorScheme.sharedInstance().barColors[colorArrayCount].setFill()
                 diamondPath.fill()
@@ -225,48 +216,8 @@ class MedsView: UIView {
             }
             sectionCount += 1
         }
-        
-        
-//        rectArray = [CGRect]()
-//        count = 0
-//        colorArrayCount = 0
-//        for event in diaryEventsFRC.fetchedObjects! {
-//            var eventRect = event.nonScoreEventRect(scale: scale).offsetBy(dx: leftXPosition(startDate: event.date as! Date, scale: scale), dy: topOfPrnMedRect)
-//            for i in 0..<count {
-//                if eventRect.intersects(rectArray[i]) {
-//                    eventRect = eventRect.offsetBy(dx: 0, dy: -eventDiamondSize - 2)
-//                }
-//            }
-//            rectArray.append(eventRect)
-//            
-//            let diamondPath = UIBezierPath()
-//            diamondPath.move(to: CGPoint(x:eventRect.midX, y: eventRect.minY))
-//            diamondPath.addLine(to: CGPoint(x:eventRect.minX, y: eventRect.midY))
-//            diamondPath.addLine(to: CGPoint(x:eventRect.midX, y: eventRect.maxY))
-//            diamondPath.addLine(to: CGPoint(x:eventRect.maxX, y: eventRect.midY))
-//            diamondPath.addLine(to: CGPoint(x:eventRect.midX, y: eventRect.minY))
-//           
-//            diamondPath.lineWidth = 1.0
-//            ColorScheme.sharedInstance().barColors[colorArrayCount].setFill()
-//            diamondPath.fill()
-//            diamondPath.stroke()
-//            
-//            let eventName = (event.name as NSString?)?.substring(to: 1)
-//            eventName?.draw(in: eventRect.offsetBy(dx: 4, dy: 0), withAttributes: [NSFontAttributeName: UIFont(name: "AvenirNext-Regular", size: fontSize)!,NSForegroundColorAttributeName: UIColor.white])
-//            
-//            count += 1
-//            colorArrayCount += 1
-//            if colorArrayCount == numberOfColors {
-//                colorArrayCount = 0
-//            }
-//        }
-        
+        symbolArrays.append(rectArray)
     }
-    
-    private func drawRegularMeds() {
-        
-    }
-    
     
     private func leftXPosition(startDate: Date, scale: TimeInterval) -> CGFloat {
         
