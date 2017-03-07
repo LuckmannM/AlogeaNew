e710c6b7286d4e99fcc4585395faaad996f24a3f
mikeMBP <mikeMBP@des.local>
2017-03-07 21:44:08 +0000

    some cleanup in EventsListVC

--- Alogea/EventsListViewController.swift
+++ Alogea/EventsListViewController.swift
@@ -37,28 +37,21 @@ class EventsListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         eventsFRC.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return eventsFRC.fetchedObjects?.count ?? 0
     }
     
@@ -71,7 +64,6 @@ class EventsListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "eventsViewCell", for: indexPath)
         var titleText = String()
         
-        print("object at path \(indexPath) is \(eventsFRC.object(at: indexPath))")
         if (eventsFRC.object(at: indexPath).vas?.doubleValue ?? -1) >= 0.0 {
             let scoreNumber = numberFormatter.string(from: eventsFRC.object(at: indexPath).vas ?? 0)
             titleText = eventsFRC.object(at: indexPath).name! + ", score: " + (scoreNumber ?? "")
@@ -140,44 +132,11 @@ class EventsListViewController: UITableViewController {
                 
                 self.deleteSelected(atPath: indexPath)
                 
-//                let deleteAlert = UIAlertController(title: "Delete this event?", message: nil, preferredStyle: .actionSheet)
-//                
-//                let proceedAction = UIAlertAction(title: "Delete", style: UIAlertActionStyle.default, handler: { (deleteAlert)
-//                    -> Void in
-//                    
-//                    let eventToDelete = self.eventsFRC.object(at: indexPath)
-//                    self.stack.context.delete(eventToDelete)
-//                    
-//                    self.save()
-//                    
-//
-//                    tableView.reloadSections([indexPath.section], with: .automatic)
-//                })
-//                
-//                let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { (deleteAlert)
-//                    -> Void in
-//                    tableView.reloadRows(at: [indexPath], with: .automatic)
-//                })
-//                
-//                deleteAlert.addAction(proceedAction)
-//                deleteAlert.addAction(cancelAction)
-//                
-//                // iPads have different requirements for AlertControllers!
-//                if UIDevice().userInterfaceIdiom == .pad {
-//                    let cell = tableView.cellForRow(at: indexPath)
-//                    let popUpController = deleteAlert.popoverPresentationController
-//                    popUpController!.permittedArrowDirections = .up
-//                    popUpController!.sourceView = self.view
-//                    popUpController!.sourceRect = (cell?.contentView.bounds)!
-//                }
-//                
-//                self.present(deleteAlert, animated: true, completion: nil)
         })
         
         deleteAction.backgroundColor = UIColor.red
         
         return [deleteAction]
-        
     }
 
 
@@ -187,7 +146,7 @@ class EventsListViewController: UITableViewController {
             try  stack.context.save()
         }
         catch let error as NSError {
-            ErrorManager.sharedInstance().errorMessage(message: "EventTypeSettings Error 1", showInVC: self, systemError: error)
+            ErrorManager.sharedInstance().errorMessage(message: "EventListVC Error 1", showInVC: self, systemError: error, errorInfo: "error when trying to save moc after event deletion")
         }
         
     }
@@ -239,55 +198,6 @@ class EventsListViewController: UITableViewController {
         self.present(deleteAlert, animated: true, completion: nil)
 
     }
-
-
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 // MARK: - FetchedResultController functions
